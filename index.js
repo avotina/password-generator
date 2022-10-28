0ea6569d450c4bd8b5ad2d79dc88c66976e9ea5a
Santa <avotina@protonmail.com>
2022-10-28 20:07:21 +0300

    large letters function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let symbolsArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V",
 "W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
 "x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")",
-"_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]
+"_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"]
+
 let renderOne = document.getElementById("render-one")
 let renderTwo = document.getElementById("render-two")
+let bigLettersCheck = document.getElementById("large-letters")
 let passwordOne = []
 let passwordTwo = []
 let passwordLength = 14
@@ -17,9 +18,18 @@ document.getElementById("generate-btn").addEventListener("click", function() {
             renderOne.textContent = passwordOne.join("")
             passwordTwo.push(randomSymbolSecond)
             renderTwo.textContent = passwordTwo.join("")
+            bigLetters()
     }
     generateNextSet()
 })
+
+function bigLetters() {
+  if (bigLettersCheck.checked) {
+    console.log("checked")
+  }
+}
+
+
 
 function generateNextSet() {
     passwordOne = []
